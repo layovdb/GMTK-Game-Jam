98aaec4c81182e6869b4aa5c3d1780a8ae716edb
layovdb <layo.vanderbauwhede@ugent.be>
2022-07-17 11:59:51 +0200

    Project toevoegen aan repository

---
+++ GMTK_Game_Jam_Roll_The_Dice/Assets/Scripts/GridManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _height, _width;
+    [SerializeField] private Tile _normalTile, _endTile;
+    [SerializeField] private Transform _cam;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                // Hier nog iets doen met endTile
+                var SpawnedTile = Instantiate(_normalTile, new Vector3(x, y), Quaternion.identity);
+                SpawnedTile.name = $"Tile {x} {y}";
+            
+                SpawnedTile.Init(x, y);
+            }
+        }
+
+        _cam.transform.position = new Vector3((float)_width/2 -0.5f, (float)_height/2 -0.5f, -10);
+    }
+}
---
+++ GMTK_Game_Jam_Roll_The_Dice/Assets/Scripts/Tiles/Tile.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Tile : MonoBehaviour
+{
+    [SerializeField] protected SpriteRenderer _renderer;
+
+    public virtual void Init(int x, int y)
+    {
+    }
+}
